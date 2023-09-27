0e91de54b040e3491ca7b9a33d57108455c84aab
Elecrow <24800915+Elecrow-RD@users.noreply.github.com>
2023-09-27 15:12:30 +0800

    Update 2.4-2.8-3.5inch_Squareline_Demo.ino

--- 3.5/3、arduino/2.4-2.8-3.5inch_Squareline_Demo/2.4-2.8-3.5inch_Squareline_Demo.ino
+++ 3.5/3、arduino/2.4-2.8-3.5inch_Squareline_Demo/2.4-2.8-3.5inch_Squareline_Demo.ino
@@ -2,8 +2,8 @@
 #include <Arduino.h>
 #include <SPI.h>
 
-//#define USE_UI
-#define Display_35
+#define USE_UI          //if you want to use the ui export from Squareline ,pleease define USE_UI.
+#define Display_35        //acording to the board you using ,if you using the ESP32 Display 3.5inch board, please define 'Display_35'.if using 2.4inch board,please define 'Display_24'.
 
 #ifdef USE_UI
 #include <lvgl.h>
@@ -11,18 +11,18 @@
 #endif
 
 #ifdef USE_UI
-#if defined Display_35
+#if defined Display_35      //ESP32 Display 3.5inch Board
 /*screen resolution*/
 static const uint16_t screenWidth  = 480;
 static const uint16_t screenHeight = 320;
 uint16_t calData[5] = { 353, 3568, 269, 3491, 7  };     /*touch caldata*/
 
-#elif defined Display_24
+#elif defined Display_24    //ESP32 Display 2.4inch Board
 static const uint16_t screenWidth  = 320;
 static const uint16_t screenHeight = 240;
 uint16_t calData[5] = { 557, 3263, 369, 3493, 3  };
 
-#elif defined Display_28
+#elif defined Display_28    //ESP32 Display 2.8inch Board
 static const uint16_t screenWidth  = 320;
 static const uint16_t screenHeight = 240;
 uint16_t calData[5] = { 189, 3416, 359, 3439, 1 };
@@ -103,17 +103,17 @@ void setup()
   
   lv_disp_draw_buf_init( &draw_buf, buf1, NULL, screenWidth * screenHeight / 13 );
 
-  /*初始化显示*/
+  /*Display init*/
   static lv_disp_drv_t disp_drv;
   lv_disp_drv_init( &disp_drv );
-  /*将以下行更改为显示分辨率*/
+  /*Display driver port of LVGL*/
   disp_drv.hor_res = screenWidth;
   disp_drv.ver_res = screenHeight;
   disp_drv.flush_cb = my_disp_flush;
   disp_drv.draw_buf = &draw_buf;
   lv_disp_drv_register( &disp_drv );
 
-  /*初始化（虚拟）输入设备驱动程序*/
+  /*touch driver port of LVGL*/
   static lv_indev_drv_t indev_drv;
   lv_indev_drv_init( &indev_drv );
   indev_drv.type = LV_INDEV_TYPE_POINTER;
@@ -121,8 +121,10 @@ void setup()
   lv_indev_drv_register( &indev_drv );
 
   ui_init();        //LVGL UI init
-#else
-  lcd.fillScreen(TFT_BLACK);
+  
+#else               //if you haven't defined 'USE_UI', the program compiler will not compile the code above; it will just compile the code follows
+
+  lcd.fillScreen(TFT_BLACK);    //The screen of the board will change the screen color per second.
   delay(1000);
   lcd.fillScreen(TFT_WHITE);
   delay(1000);
